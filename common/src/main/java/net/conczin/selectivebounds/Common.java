df281ca0b93fc92f92d6540f3a6c33037ed5a3ac
Luke100000 <jonas.peche@aon.at>
2025-05-03 13:21:45 +0200

    refactor

--- common/src/main/java/net/conczin/selectivebounds/Common.java
+++ common/src/main/java/net/conczin/selectivebounds/Common.java
@@ -1,5 +1,12 @@
 package net.conczin.selectivebounds;
 
+import net.conczin.selectivebounds.config.Config;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.state.BlockState;
+
 public class Common {
     private static long lastInteractTime;
     public static final String MOD_ID = "selectivebounds";
@@ -14,5 +21,26 @@ public class Common {
 
     public static long getLastInteractTime() {
         return lastInteractTime;
+    }
+
+    public static boolean shouldShowOutline(Entity entity, BlockPos blockPos, BlockState blockState, Player player) {
+        Config c = Config.getInstance();
+
+        // Sneaking
+        if (c.showOnSneak && entity.isShiftKeyDown()) return false;
+
+        // A block has been placed or broken recently
+        long diff = System.currentTimeMillis() - Common.getLastInteractTime();
+        if (c.showOnAction && diff < c.showOnActionTime * 1000) return false;
+
+        Item tool = player.getItemInHand(player.getUsedItemHand()).getItem();
+
+        // The current tool cannot break the block
+        if (c.considerCanAttackBlock && !tool.canAttackBlock(blockState, player.level(), blockPos, player)) {
+            return true;
+        }
+
+        // The current tool is not the correct tool for the block
+        return c.considerIsCorrectToolForDrops && !tool.isCorrectToolForDrops(blockState);
     }
 }
--- common/src/main/java/net/conczin/selectivebounds/mixin/LevelRendererMixin.java
+++ common/src/main/java/net/conczin/selectivebounds/mixin/LevelRendererMixin.java
@@ -19,27 +19,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class LevelRendererMixin {
     @Inject(method = "renderHitOutline(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/entity/Entity;DDDLnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)V", at = @At("HEAD"), cancellable = true)
     private void selectivebounds$renderHitOutline(PoseStack poseStack, VertexConsumer vertexConsumer, Entity entity, double d, double e, double f, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
-        if (entity instanceof Player player) {
-            Config c = Config.getInstance();
-
-            // Sneaking
-            if (c.showOnSneak && entity.isShiftKeyDown()) return;
-
-            // A block has been placed or broken recently
-            long diff = System.currentTimeMillis() - Common.getLastInteractTime();
-            if (c.showOnAction && diff < c.showOnActionTime * 1000) return;
-
-            Item tool = player.getItemInHand(player.getUsedItemHand()).getItem();
-
-            // The current tool can not break the block
-            if (c.considerCanAttackBlock && !tool.canAttackBlock(blockState, player.level(), blockPos, player)) {
-                ci.cancel();
-            }
-
-            // The current tool is not the correct tool for the block
-            if (c.considerIsCorrectToolForDrops && !tool.isCorrectToolForDrops(blockState)) {
-                ci.cancel();
-            }
+        if (entity instanceof Player player && Common.shouldShowOutline(entity, blockPos, blockState, player)) {
+            ci.cancel();
         }
     }
+
 }
